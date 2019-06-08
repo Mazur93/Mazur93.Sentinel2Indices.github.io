ed19b5400155826aec240f32b59918f12ad71013
Mazur93 <bartoszmazurkiewicz899@gmail.com>
2019-06-08 21:45:46 +0200

    Added NDWI and MNDWI

--- Indexes.py
+++ Indexes.py
@@ -1,3 +1,10 @@
+class SatelliteIndex:
+    def __init__(self,name):
+        self.name = name
+
+
+
+
 indexes = {}
 
 
@@ -11,9 +18,29 @@ def build_simple_index(channel1, channel2):
 
 
 #Normalized Difference Vegetation Index
-NDVI={}
-NDVI['name'] = 'Normalized Difference Vegetation Index'
-NDVI['channels'] = ['B08','B04']
-NDVI['function'] = build_simple_index
-NDVI['use'] = 'Detection of vegetation'
-NDVI['Source'] = 'https://www.sentinel-hub.com/eoproducts/ndvi-normalized-difference-vegetation-index'
+NDVI = SatelliteIndex('Normalized Difference Vegetation Index')
+NDVI.abbreviation = 'NDVI'
+NDVI.channels = ['B08', 'B04']
+NDVI.function = build_simple_index
+NDVI.use = 'Detection of vegetation'
+NDVI.Source = 'https://www.sentinel-hub.com/eoproducts/ndvi-normalized-difference-vegetation-index'
+indexes['NDVI'] = NDVI
+
+#Normalized Difference Water Index
+NDWI = SatelliteIndex('Normalized Difference Water Index')
+NDWI.abbreviation = 'NDWI'
+NDWI.channels = ['B08', 'B11']
+NDWI.function = build_simple_index
+NDWI.use = 'Detection of water'
+NDWI.Source = 'https://www.sentinel-hub.com/develop/documentation/eo_products/Sentinel2EOproducts'
+indexes['NDWI'] = NDWI
+
+#Modified Normalized Difference Water Index
+MNDWI = SatelliteIndex('Modified Normalized Difference Water Index')
+MNDWI.abbreviation = 'MNDWI'
+MNDWI.channels = ['B03', 'B11']
+MNDWI.function = build_simple_index
+MNDWI.use = 'Detection of water'
+MNDWI.Source = 'https://www.mdpi.com/2072-4292/8/4/354'
+indexes['MNDWI'] = MNDWI
+
